1dce41f653674fac05d37c101b504c317cf223b6
Gasipe <gblsilvap@gmail.com>
2026-06-25 16:11:35 -0300

    arrumando a estrutura do projeto

--- frontend/src/app/core/service/todo.service.ts
+++ frontend/src/app/core/service/todo.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { HttpResponse } from '@angular/common/http';
-import { responseToDoDTO, createToDoDTO, updateToDoDTO } from '../model/dto/todo.dtos';
+import { responseToDoDTO, createToDoDTO, updateToDoDTO } from '../dto/todo.dtos';
 
 @Injectable({
   providedIn: 'root',
