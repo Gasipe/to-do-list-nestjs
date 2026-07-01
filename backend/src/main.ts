84ed7a8d00ab2a549f87bba3520d683e9cd998dc
Gasipe <gblsilvap@gmail.com>
2026-07-01 20:22:11 -0300

    Foi instalado o pacote cors no backend, e reconfigurado para que seja possivel receber os dados do frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,9 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
-
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 }
 bootstrap();
